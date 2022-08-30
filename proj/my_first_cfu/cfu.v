2d1067471f25d355a4ea45fef57234c5c957cc0f
tyhsiao <pudding88888@gmail.com>
2022-08-30 20:30:06 +0800

    moved input data into SRAM

--- proj/my_first_cfu/cfu.v
+++ proj/my_first_cfu/cfu.v
@@ -13,7 +13,7 @@ module Cfu (
 
 
 localparam FILTER_DATA_SIZE = 65536;
-localparam INPUT_DATA_SIZE = 18435;
+localparam INPUT_DATA_ENTRY = 18435;
 localparam DATA_LEN = 8;
 
 // Dismantle Function Code
@@ -45,25 +45,40 @@ assign cmd_ready = ( ~rsp_valid && ~acc_start && ~cal_finish) ;
 
 
 //***** SRAM *****//
+// reg [$clog2(FILTER_DATA_SIZE)-1: 0] filtersram_addr [0:7];
+// reg [DATA_LEN-1: 0]                 filtersram_data [0:7];
 // sram #( .DATA_LEN(DATA_LEN), .N_ENTRIES(FILTER_DATA_SIZE) )
 //       FILTER_BRAM(
 //           .clk(clk),
 //           .en_i(1'b1),
 //           .we_i(1),
-//           .addr_i(1),
-//           .data_i(1),
+//           .addr_i0(filtersram_addr[0]), .addr_i1(filtersram_addr[1]), .addr_i2(filtersram_addr[2]), .addr_i3(filtersram_addr[3]),
+//           .addr_i4(filtersram_addr[4]), .addr_i5(filtersram_addr[5]), .addr_i6(filtersram_addr[6]), .addr_i7(filtersram_addr[7]),
+//           .data_i0(filtersram_data[0]), .data_i1(filtersram_data[1]), .data_i2(filtersram_data[2]), .data_i3(filtersram_data[3]),
+//           .data_i4(filtersram_data[4]), .data_i5(filtersram_data[5]), .data_i6(filtersram_data[6]), .data_i7(filtersram_data[7]),
 //           .data_o(1)
 //       );
 
-// sram #( .DATA_LEN(DATA_LEN), .N_ENTRIES(INPUT_DATA_SIZE) )
-//       DATA_BRAM(
-//           .clk(clk),
-//           .en_i(1'b1),
-//           .we_i(1),
-//           .addr_i(1),
-//           .data_i(1),
-//           .data_o(1)
-//       );      
+//***** SRAM FOR DATA *****//
+reg  [$clog2(INPUT_DATA_ENTRY)-1: 0] inputsram_addr [0:7];
+wire [$clog2(INPUT_DATA_ENTRY)-1: 0] inputsram_addr_wire;
+reg  [DATA_LEN-1: 0]                 inputsram_data [0:7];
+wire [DATA_LEN-1: 0]                 outputsram_data ;
+reg  [7:0]                           we_data;
+
+
+sram #( .DATA_LEN(DATA_LEN), .N_ENTRIES(INPUT_DATA_ENTRY) )
+      DATA_BRAM(
+          .clk(clk),
+          .reset(reset),
+          .en_i(1'b1),
+          .we_i(we_data),
+          .addr_i0(inputsram_addr_wire), .addr_i1(inputsram_addr[1]), .addr_i2(inputsram_addr[2]), .addr_i3(inputsram_addr[3]),
+          .addr_i4(inputsram_addr[4]), .addr_i5(inputsram_addr[5]), .addr_i6(inputsram_addr[6]), .addr_i7(inputsram_addr[7]),
+          .data_i0(inputsram_data[0]), .data_i1(inputsram_data[1]), .data_i2(inputsram_data[2]), .data_i3(inputsram_data[3]),
+          .data_i4(inputsram_data[4]), .data_i5(inputsram_data[5]), .data_i6(inputsram_data[6]), .data_i7(inputsram_data[7]),
+          .data_o(outputsram_data)
+      );      
 
 //***** Set Conv Parameters *****//
 reg signed [31:0] input_offset, input_height, input_width, input_batches, input_depth;
@@ -256,7 +271,12 @@ end
 
 // Main Acc
 //index method ((i0 * dims_data[1] + i1) * dims_data[2] + i2) * dims_data[3] + i3;
-wire [31:0] input_index, filter_index; 
+wire [31:0] input_index, filter_index;
+reg  [31:0] filter_index_delay;
+
+always@ (posedge clk) begin
+  filter_index_delay <= filter_index;
+end
 
 assign input_index = 
   ((((batch * input_height + in_y) * input_width ) + in_x) * input_depth ) + (in_channel + group * filter_input_depth);
@@ -264,18 +284,17 @@ assign input_index =
 assign filter_index = 
   ((((out_channel * filter_height + filter_y) * filter_width ) + filter_x) * filter_input_depth ) + (in_channel);
 
-
+// cal_now_delay for the delay output for SRAM
 always @(posedge clk or posedge reset) begin
   if(reset) rsp_payload_outputs_0 <= 32'b0;
   else if( cmd_valid && func7 == 7'd8 ) rsp_payload_outputs_0 <= 32'b0;
-  else if (cal_now) rsp_payload_outputs_0 <= rsp_payload_outputs_0 + prod_0;
+  else if (cal_now_delay) rsp_payload_outputs_0 <= rsp_payload_outputs_0 + prod_0;
 end
 
 // SIMD multiply step:
 wire signed [31:0] prod_0;
-assign prod_0 =  (($signed(input_data[input_index]) + $signed(input_offset))
-                 * $signed(filter_data[filter_index]));
-
+assign prod_0 =  (($signed(outputsram_data) + $signed(input_offset))
+                 * $signed(filter_data[filter_index_delay]));
 
 
 //*****     Get Filter Data     *****//
@@ -318,39 +337,63 @@ end
 
 //*****     Get Input Data     *****//
 
-reg signed [7:0] input_data [0:INPUT_DATA_SIZE];
 reg [31:0] ifilt;
+
+assign inputsram_addr_wire = (cal_now) ? input_index : inputsram_addr[0];
 
 always @(posedge clk or posedge reset) begin
   if (reset) begin 
-    input_data <= '{default: '0}; 
     ifilt <= 0;
+    we_data <= 0;
+
   end
 
   else if (cmd_valid) begin
     if (func7 == 7'd3) begin
-      input_data <= '{default: '0}; 
       ifilt <= 0;
+      we_data <= 0;
+
+    end
+
+    else if(cal_now) begin
+      we_data <= 0;
     end
 
     else if (func7 == 7'd4) begin
+      inputsram_addr[0] <= ifilt;
+      inputsram_addr[1] <= ifilt + 1;
+      inputsram_addr[2] <= ifilt + 2;
+      inputsram_addr[3] <= ifilt + 3;
+      inputsram_addr[4] <= ifilt + 4;
+      inputsram_addr[5] <= ifilt + 5;
+      inputsram_addr[6] <= ifilt + 6;
+      inputsram_addr[7] <= ifilt + 7;
+
       // Change the order of memory due to little endian system
-      input_data[ifilt  ] <= $signed(cmd_payload_inputs_0[ 7: 0]);
-      input_data[ifilt+1] <= $signed(cmd_payload_inputs_0[15: 8]);
-      input_data[ifilt+2] <= $signed(cmd_payload_inputs_0[23:16]);
-      input_data[ifilt+3] <= $signed(cmd_payload_inputs_0[31:24]);
-      input_data[ifilt+4] <= $signed(cmd_payload_inputs_1[ 7: 0]);
-      input_data[ifilt+5] <= $signed(cmd_payload_inputs_1[15: 8]);
-      input_data[ifilt+6] <= $signed(cmd_payload_inputs_1[23:16]);
-      input_data[ifilt+7] <= $signed(cmd_payload_inputs_1[31:24]);
+      inputsram_data[0] <= $signed(cmd_payload_inputs_0[ 7: 0]);
+      inputsram_data[1] <= $signed(cmd_payload_inputs_0[15: 8]);
+      inputsram_data[2] <= $signed(cmd_payload_inputs_0[23:16]);
+      inputsram_data[3] <= $signed(cmd_payload_inputs_0[31:24]);
+      inputsram_data[4] <= $signed(cmd_payload_inputs_1[ 7: 0]);
+      inputsram_data[5] <= $signed(cmd_payload_inputs_1[15: 8]);
+      inputsram_data[6] <= $signed(cmd_payload_inputs_1[23:16]);
+      inputsram_data[7] <= $signed(cmd_payload_inputs_1[31:24]);
+
+      we_data <= 8'b1111_1111;
       ifilt <= ifilt + 6'd8;
     end
 
     else if (func7 == 7'd5) begin
-      input_data[ifilt] <= $signed(cmd_payload_inputs_0[31:24]);
+      inputsram_addr[0] <= ifilt;
+      inputsram_data[0] <= $signed(cmd_payload_inputs_0[ 7: 0]);
+      we_data <= 7'b0000_0001;
       ifilt <= ifilt + 1;
     end
+
+    else we_data <= 0;
   end
+
+  else  we_data <= 0;
  
 end
 
--- proj/my_first_cfu/sram.v
+++ proj/my_first_cfu/sram.v
@@ -1,11 +1,27 @@
 module sram
-#(parameter DATA_LEN = 32, N_ENTRIES = 1024)
+#(parameter DATA_LEN = 8, N_ENTRIES = 10240)
 (
     input                           clk,
     input                           en_i,
-    input                           we_i,
-    input  [$clog2(N_ENTRIES)-1: 0] addr_i,
-    input  [DATA_LEN-1: 0]        data_i,
+    input                           reset,
+    input  [7:0]                    we_i,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i0,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i1,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i2,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i3,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i4,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i5,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i6,
+    input  [$clog2(N_ENTRIES)-1: 0] addr_i7,
+
+    input  [DATA_LEN-1: 0]        data_i0,
+    input  [DATA_LEN-1: 0]        data_i1,
+    input  [DATA_LEN-1: 0]        data_i2,
+    input  [DATA_LEN-1: 0]        data_i3,
+    input  [DATA_LEN-1: 0]        data_i4,
+    input  [DATA_LEN-1: 0]        data_i5,
+    input  [DATA_LEN-1: 0]        data_i6,
+    input  [DATA_LEN-1: 0]        data_i7,
     output reg [DATA_LEN-1: 0]    data_o
 );
 
@@ -14,23 +30,29 @@ reg [DATA_LEN-1 : 0] RAM [N_ENTRIES-1: 0];
 // ------------------------------------
 // Read operation
 // ------------------------------------
-always@(posedge clk)
-begin
-    if (en_i)
-    begin
-        data_o <= RAM[addr_i];
+always@(posedge clk) begin
+    if (en_i) begin
+        data_o <= RAM[addr_i0];
     end
 end
 
 // ------------------------------------
 // Write operation
 // ------------------------------------
-always@(posedge clk)
-begin
-    if (en_i & we_i)
-    begin
-        RAM[addr_i] <= data_i;
+always@(posedge clk or posedge reset) begin
+    if (reset)  RAM <= '{default: '0}; 
+
+    else begin
+        if (en_i & we_i[7]) RAM[addr_i7] <= data_i7;
+        if (en_i & we_i[6]) RAM[addr_i6] <= data_i6;
+        if (en_i & we_i[5]) RAM[addr_i5] <= data_i5;
+        if (en_i & we_i[4]) RAM[addr_i4] <= data_i4;
+        if (en_i & we_i[3]) RAM[addr_i3] <= data_i3;
+        if (en_i & we_i[2]) RAM[addr_i2] <= data_i2;
+        if (en_i & we_i[1]) RAM[addr_i1] <= data_i1;
+        if (en_i & we_i[0]) RAM[addr_i0] <= data_i0;
     end
 end
+
 
 endmodule
